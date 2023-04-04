7cd4bed0a4d1a0f2d75b79b074a8175344fb3e86
Maxime <maxilefranc@gmail.com>
2023-04-04 19:10:47 +0200

    Setting the minimum number of characters for a product

--- App.js
+++ App.js
@@ -1,47 +1,51 @@
 import { useState } from "react";
-import { StyleSheet, View, FlatList } from "react-native";
+import { StyleSheet, View, FlatList, Alert } from "react-native";
 
 // Component
 import Products from "./src/Components/Products.js";
 import AddProduct from "./src/Components/AddProduct.js";
 
 export default function App() {
-  const [myProducts, setMyProducts] = useState([]);
+	const [myProducts, setMyProducts] = useState([]);
 
-  const submitHandler = (product) => {
-    const idString = Date.now().toString();
-    setMyProducts((currentMyProduct) => [
-      { key: idString, name: product },
-      ...currentMyProduct,
-    ]);
-  };
+	const submitHandler = (product) => {
+		if (product.length > 1) {
+			const idString = Date.now().toString();
+			setMyProducts((currentMyProduct) => [
+				{ key: idString, name: product },
+				...currentMyProduct,
+			]);
+		} else {
+			Alert.alert("Désolé", "Le nombre de caractère minimum est 2.");
+		}
+	};
 
-  const deleteProduct = (key) => {
-    setMyProducts((currentMyProducts) => {
-      return currentMyProducts.filter((product) => product.key !== key);
-    });
-  };
+	const deleteProduct = (key) => {
+		setMyProducts((currentMyProducts) => {
+			return currentMyProducts.filter((product) => product.key !== key);
+		});
+	};
 
-  return (
-    <View style={styles.container}>
-      <AddProduct submitHandler={submitHandler} />
-      <FlatList
-        data={myProducts}
-        renderItem={({ item }) => (
-          <Products
-            name={item.name}
-            deleteProduct={deleteProduct}
-            id={item.key}
-          />
-        )}
-      />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<AddProduct submitHandler={submitHandler} />
+			<FlatList
+				data={myProducts}
+				renderItem={({ item }) => (
+					<Products
+						name={item.name}
+						deleteProduct={deleteProduct}
+						id={item.key}
+					/>
+				)}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 20,
-    paddingTop: 50,
-  },
+	container: {
+		padding: 20,
+		paddingTop: 50,
+	},
 });
--- src/Components/AddProduct.js
+++ src/Components/AddProduct.js
@@ -1,84 +1,92 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
-  StyleSheet,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Text,
+	StyleSheet,
+	View,
+	TextInput,
+	TouchableOpacity,
+	Text,
 } from "react-native";
 import PropTypes from "prop-types";
 
 const AddProduct = ({ submitHandler }) => {
-  const [product, setProduct] = useState("");
+	const [product, setProduct] = useState("");
+	const [btnDisable, setBtnDisable] = useState(true);
 
-  const inputHandler = (value) => {
-    setProduct(value);
-  };
+	useEffect(() => {
+		if (product.length > 1) {
+			setBtnDisable(false);
+		} else {
+			setBtnDisable(true);
+		}
+	}, [product]);
 
-  const clickHandler = () => {
-    if (product) {
-      submitHandler(product);
-      setProduct("");
-    }
-  };
+	const inputHandler = (value) => {
+		setProduct(value);
+	};
 
-  return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        style={styles.textInput}
-        placeholder="Nouveau produit"
-        onChangeText={inputHandler}
-        value={product}
-        clearButtonMode="while-editing"
-        onSubmitEditing={clickHandler}
-        autoFocus={true}
-      />
-      <TouchableOpacity
-        style={styles.submitButton}
-        title="valider"
-        onPress={clickHandler}
-      >
-        <Text style={styles.submitText}>Valider</Text>
-      </TouchableOpacity>
-    </View>
-  );
+	const clickHandler = () => {
+		submitHandler(product);
+		setProduct("");
+	};
+
+	return (
+		<View style={styles.inputContainer}>
+			<TextInput
+				style={styles.textInput}
+				placeholder="Nouveau produit"
+				onChangeText={inputHandler}
+				value={product}
+				clearButtonMode="while-editing"
+				onSubmitEditing={clickHandler}
+				autoFocus={true}
+			/>
+			<TouchableOpacity
+				style={styles.submitButton}
+				title="valider"
+				onPress={clickHandler}
+				disabled={btnDisable}
+			>
+				<Text style={styles.submitText}>Valider</Text>
+			</TouchableOpacity>
+		</View>
+	);
 };
 
 AddProduct.propTypes = {
-  submitHandler: PropTypes.func.isRequired,
+	submitHandler: PropTypes.func.isRequired,
 };
 
 const styles = StyleSheet.create({
-  inputContainer: {
-    flexDirection: "row",
-    marginBottom: 15,
-  },
-  textInput: {
-    borderColor: "grey",
-    borderRightWidth: 0,
-    borderWidth: 1,
-    padding: 5,
-    paddingLeft: 9,
-    fontSize: 18,
-    flexGrow: 1,
-    borderBottomLeftRadius: 10,
-    borderTopLeftRadius: 10,
-  },
-  submitButton: {
-    borderWidth: 1,
-    borderColor: "lightblue",
-    borderTopRightRadius: 10,
-    borderBottomRightRadius: 10,
-    overflow: "hidden",
-    backgroundColor: "lightblue",
-    justifyContent: "center",
-    alignItems: "center",
-    width: 80,
-  },
-  submitText: {
-    color: "white",
-    fontSize: 18,
-  },
+	inputContainer: {
+		flexDirection: "row",
+		marginBottom: 15,
+	},
+	textInput: {
+		borderColor: "grey",
+		borderRightWidth: 0,
+		borderWidth: 1,
+		padding: 5,
+		paddingLeft: 9,
+		fontSize: 18,
+		flexGrow: 1,
+		borderBottomLeftRadius: 10,
+		borderTopLeftRadius: 10,
+	},
+	submitButton: {
+		borderWidth: 1,
+		borderColor: "lightblue",
+		borderTopRightRadius: 10,
+		borderBottomRightRadius: 10,
+		overflow: "hidden",
+		backgroundColor: "lightblue",
+		justifyContent: "center",
+		alignItems: "center",
+		width: 80,
+	},
+	submitText: {
+		color: "white",
+		fontSize: 18,
+	},
 });
 
 export default AddProduct;
